68c5b9bd9fa8d508bbeb975ac66b95d7a495e8ff
ghtpdkzz <ghtpdkzz@naver.com>
2016-04-28 23:28:18 +0900

    Hoase - User is changed

--- src/main/java/com/coawesome/persistence/BoardMapper.java
+++ src/main/java/com/coawesome/persistence/BoardMapper.java
@@ -16,11 +16,12 @@ public interface BoardMapper {
   @Insert("INSERT INTO user(login_id, password, name) VALUES(#{login_id}, #{password}, #{name})")
   void addUser(User user);
 
+   @Select("select  from user where user_id = #{login_id}")
+    Usedddasdfasr (@Param("login_id") String )
+
   @Insert("INSERT INTO board(user_id, title, content) VALUES(#{user_id}, #{title}, #{content})")
     void insertBoard(BoardVO board);
 
-
-
-    @Select("select * from board where board_id = #{board_id}")
+  @Select("select * from board where board_id = #{board_id}")
     BoardVO findById(@Param("board_id") int board_id);
 }
