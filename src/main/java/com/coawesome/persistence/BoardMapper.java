8e049de53c4d9e062f95645d6b21adb0a2e6f8a0
ghtpdkzz <ghtpdkzz@naver.com>
2016-05-03 22:36:08 +0900

    Hoaseaa

--- src/main/java/com/coawesome/persistence/BoardMapper.java
+++ src/main/java/com/coawesome/persistence/BoardMapper.java
@@ -8,6 +8,8 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by eastflag on 2016-04-25.
@@ -27,19 +29,30 @@ public interface BoardMapper {
   @Select("select password from user where login_id = #{login_id} and email = #{email}")
   String findPASS(User user);
 
+
+
   //로그인
   @Select("select password from user where login_id = #{login_id}")
   String Login(User user);
 
   //게시글 등록
   @Insert("INSERT INTO board(user_id, public_level, tag1, tag2, tag3, line1, line1_x, line1_y, line2, line2_x, line2_y) VALUES(#{user_id}, #{public_level}, #{tag1}, #{tag2}, #{tag3}, #{line1}, #{line1_x}, #{line1_y}, #{line2}, #{line2_x}, #{line2_y})")
-    void insertBoard(BoardVO board);
+    void insertBoard(Map board);
+//  @Insert("INSERT INTO board(board_id, original_file_name, stored_file_name, file_size) VALUES()")
+//  void insertBoardImage(HashMap board);
+  @Insert("INSERT INTO board(board_id, original_file_name, stored_file_name, file_size) VALUES(#{board_id}, #{original_file_name}, #{stored_file_name}, #{file_size})")
+  void insertFile(Map board);
+
+
 
   //게시판 글 목록 조회 TODO 디테일 작업전
-  @Select("select * from board where user_id = #{user_id}")
-  ArrayList<BoardVO> boardfindById(@Param("user_id") int user_id);
+  @Select("select * from board left join board_image on board.board_id = board_image.board_id")
+  ArrayList<HashMap> getBoardById(@Param("user_id") int user_id);
 
-    //게시글 보기 TODO 디테일 작업전
-    @Select("select * from board where board_id = #{board_id}")
-    BoardVO findById(@Param("board_id") int board_id);
+
+
+
+  //게시글 보기 TODO 디테일 작업전
+  @Select("select * from board where board_id = #{board_id}")
+  BoardVO findById(@Param("board_id") int board_id);
 }
