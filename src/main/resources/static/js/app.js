24d9a4f6ed6249a7a8ba9876e56b23eae26cf4d6
thesun4sky <thesun4sky@gmail.com>
2016-05-31 15:31:53 +0900

    1. 테그페이지 게시글 목록 수정 2. 테그 페이지 상단배경이미지 및 사진 보더 정의

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -343,6 +343,7 @@ angular.module("homeApp",[
 
     .controller("loginCtrl",function($scope, $http, store, $state, $filter){
 
+
         $scope.login=[{
             login_id :"" ,password :""
         }];
@@ -351,7 +352,7 @@ angular.module("homeApp",[
         $scope.logout = function(){
             store.set('obj',null);
             $state.go('login');
-        }
+        };
 
 
 
@@ -394,6 +395,7 @@ angular.module("homeApp",[
                     console.log(status);
                 });
         };
+
     })
 
     .controller("joinCtrl",function($scope, $http, $state){
@@ -677,7 +679,16 @@ angular.module("homeApp",[
     })
 
     .controller("findCtrl",function($rootScope,$scope,$http, store, $state){
+
+
         $scope.find_user = "";
+
+
+        $scope.logout = function(){
+            store.set('obj',null);
+            $state.go('login');
+        };
+
 
         $scope.keyHit = function() {
 
@@ -699,7 +710,25 @@ angular.module("homeApp",[
         $scope.toTag = function(tags){
             $rootScope.tag_name = tags.tag;
             $state.go("tag");
-        }
+        };
+
+
+        var userObject = store.get('obj');
+
+        $scope.user_Object=[{
+            user_id : userObject.user_id,
+            user_img : "",
+            name : userObject.name
+        }];
+        $http({
+            method: 'POST', //방식
+            url: "/user/loadProfile", /* 통신할 URL */
+            data: userObject, /* 파라메터로 보낼 데이터 */
+            headers: {'Content-Type': 'application/json; charset=utf-8'} //헤더
+        })
+            .then(function(response) {
+                $scope.user_Object = response.data;
+            });
     })
 
 
@@ -938,7 +967,7 @@ angular.module("homeApp",[
     })
 
     //테그 프로필페이지 컨트롤러
-    .controller("tagCtrl",function($rootScope,$scope,$http, store, $state) {
+    .controller("tagCtrl",function($rootScope,$scope,$http, store, $uibModal, $state) {
         var userObject = store.get('obj');
         $scope.tag_name = $rootScope.tag_name;
 
@@ -947,6 +976,8 @@ angular.module("homeApp",[
             user_id : userObject.user_id,
             tag : $scope.tag_name
         };
+
+
         $http({  //TODO 테그정보 가져오기(사진 등..)
             method: 'POST', //방식
             url: "/tag/loadTagProfile", /* 통신할 URL */
@@ -955,19 +986,20 @@ angular.module("homeApp",[
         })
             .then(function (response) {
                 $scope.tagProfile = response.data;
-                alert($scope.tagProfile.stored_file_name);
             });
+        $scope.tagBoardList = function () {
+            $http({  //TODO 테그된 게시글 가져오기
+                method: 'POST', //방식
+                url: "/tag/openTagFolder", /* 통신할 URL */
+                data: tagObject, /* 파라메터로 보낼 데이터 */
+                headers: {'Content-Type': 'application/json; charset=utf-8'} //헤더
+            })
+                .then(function (response) {
+                    $scope.my_tagBoards = response.data;
+                });
+        };
 
-        $http({  //TODO 테그된 게시글 가져오기
-            method: 'POST', //방식
-            url: "/tag/openTagFolder", /* 통신할 URL */
-            data: tagObject, /* 파라메터로 보낼 데이터 */
-            headers: {'Content-Type': 'application/json; charset=utf-8'} //헤더
-        })
-            .then(function (response) {
-                $scope.my_tagBoards = response.data;
-            });
-
+        $scope.tagBoardList();
 
         $scope.tagCloud = function () {
             $http({  //TODO 테그된 게시글 가져오기
@@ -990,6 +1022,150 @@ angular.module("homeApp",[
         $scope.tagCloud();
 
         $rootScope.tag_name={};
+
+
+        $scope.folderList = function () {
+            $http({
+                method: 'POST', //방식
+                url: "/api/folderlist", /* 통신할 URL */
+                data: userObject, /* 파라메터로 보낼 데이터 */
+                headers: {'Content-Type': 'application/json; charset=utf-8'} //헤더
+            })
+                .then(function (response) {
+                    $scope.folders = response.data;
+                })
+        };
+        $scope.folderList();
+
+        $scope.putInFolder = function (board_id, folder_id) {
+            var folderObject = {
+                board_id : board_id,
+                folder_id : folder_id
+            };
+            $http({
+                method: 'POST', //방식
+                url: "/api/putInFolder", /* 통신할 URL */
+                data: folderObject, /* 파라메터로 보낼 데이터 */
+                headers: {'Content-Type': 'application/json; charset=utf-8'} //헤더
+            })
+                .then(function () {
+                    alert('폴더에 담겼습니다.');
+                })
+        };
+
+        $scope.open = function (size, board) {
+            $rootScope.modalboard=board;
+            $scope.replylist($rootScope.modalboard);
+            var modalInstance = $uibModal.open({
+                templateUrl: 'modal.html',
+                controller: 'ModalDemoCtrl',
+                size: size
+            });
+            modalInstance.result.then(function () {
+
+            }, function () {
+                // $log.info('Modal dismissed at: ' + new Date());
+                $rootScope.modalboard={};
+                $rootScope.reply={};
+                $scope.tagBoardList();
+            });
+        };
+
+
+        $scope.favoriteBoard = function (board_id) {
+            $http({
+                method: 'POST', //방식
+                url: "/api/favoriteBoard", /* 통신할 URL */
+                data: {user_id: userObject.user_id, board_id: board_id}, /* 파라메터로 보낼 데이터 */
+                headers: {'Content-Type': 'application/json; charset=utf-8'} //헤더
+            })
+                .success(function (data, status, headers, config) {
+                    if (data.msg == 'false') {
+                        alert('페이버릿 실패')
+                    } else {
+                        $scope.tagBoardList();
+                    }
+                })
+                .error(function (data, status, headers, config) {
+                    /* 서버와의 연결이 정상적이지 않을 때 처리 */
+                    console.log(status);
+                });
+        };
+
+
+        $scope.UnfavoriteBoard = function (board_id) {
+            var UnfavoriteBoardObject =
+            {
+                user_id: userObject.user_id, //임시로 1번사용자 지정
+                board_id: board_id
+            };
+            $http({
+                method: 'POST', //방식
+                url: "/api/UnfavoriteBoard", /* 통신할 URL */
+                data: UnfavoriteBoardObject, /* 파라메터로 보낼 데이터 */
+                headers: {'Content-Type': 'application/json; charset=utf-8'} //헤더
+            })
+                .success(function (data, status, headers, config) {
+                    if (data.msg == 'false') {
+                        alert('페이버릿취소 실패')
+                    } else {
+                        $scope.tagBoardList();
+                    }
+                })
+                .error(function (data, status, headers, config) {
+                    /* 서버와의 연결이 정상적이지 않을 때 처리 */
+                    console.log(status);
+                });
+        };
+
+
+        //좋아요
+        $scope.likeBoard = function (board_id) {
+            $http({
+                method: 'POST', //방식
+                url: "/api/likeBoard", /* 통신할 URL */
+                data: {user_id: userObject.user_id, board_id: board_id}, /* 파라메터로 보낼 데이터 */
+                headers: {'Content-Type': 'application/json; charset=utf-8'} //헤더
+            })
+                .success(function (data, status, headers, config) {
+                    if (data.msg == 'false') {
+                        alert('좋아요 실패')
+                    } else {
+                        $scope.tagBoardList();
+                    }
+                })
+                .error(function (data, status, headers, config) {
+                    /* 서버와의 연결이 정상적이지 않을 때 처리 */
+                    console.log(status);
+                });
+        };
+
+        //좋아요 취소
+        $scope.dislikeBoard = function (board_id) {
+            var dislikeBoardObject =
+            {
+                user_id: userObject.user_id, //임시로 1번사용자 지정
+                board_id: board_id
+            };
+            $http({
+                method: 'POST', //방식
+                url: "/api/dislikeBoard", /* 통신할 URL */
+                data: dislikeBoardObject, /* 파라메터로 보낼 데이터 */
+                headers: {'Content-Type': 'application/json; charset=utf-8'} //헤더
+            })
+                .success(function (data, status, headers, config) {
+                    if (data.msg == 'false') {
+                        alert('좋아요취소 실패')
+                    } else {
+                        $scope.tagBoardList();
+                    }
+                })
+                .error(function (data, status, headers, config) {
+                    /* 서버와의 연결이 정상적이지 않을 때 처리 */
+                    console.log(status);
+                });
+        };
+
     })
 
 
@@ -1092,7 +1268,7 @@ angular.module("homeApp",[
     .controller("indexCtrl",function($interval, $scope, $http, store, $state, $uibModal, $rootScope, $filter) {
         var userObject = store.get('obj');
         var timeObject ={};
-        //TODO: 로그인 정보를 토큰에서 받는것으로 변경하기
+        $scope.logoWidth = window.innerWidth/6;
 
         $scope.boardList = function () {
             $http({
@@ -1122,7 +1298,6 @@ angular.module("homeApp",[
                 .then(function (response) {
                 })
         };
-        $scope.updateTime();
         $scope.folderList = function () {
             $http({
                 method: 'POST', //방식
