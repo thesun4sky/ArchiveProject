4ed8f84a77c68c1fae0367ac46dd53998ce583a0
thesun4sky <123xodid>
2016-06-02 12:31:34 +0900

    페이지전체적인 아이콘수정 및 카테고리메뉴 화면크기에 반응하게 수정

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -339,6 +339,36 @@ angular.module("homeApp",[
             return output;
         };
     })
+        //윈도우 크기 재구성
+    .directive('resize', function ($window) {
+        return function (scope, element) {
+            var w = angular.element($window);
+            scope.getWindowDimensions = function () {
+                return {
+                    'h': w.height(),
+                    'w': w.width()
+                };
+            };
+            scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+                scope.windowHeight = newValue.h;
+                scope.windowWidth = newValue.w;
+
+                scope.style = function () {
+                    return {
+                        'height': (newValue.h - 100) + 'px',
+                        'width': (newValue.w - 100) + 'px'
+                    };
+                };
+
+            }, true);
+
+            w.bind('resize', function () {
+                scope.$apply();
+            });
+        }
+    })
+
+
 
     // 댓글 엔터 입력 처리
     .directive('ngEnter', function () {
