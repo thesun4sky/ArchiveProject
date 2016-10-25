71f4ead18dfddc197deebb6c84c509f173cf6e10
thesun4sky <thesun4sky@gmail.com>
2016-10-25 18:18:41 +0900

    무한스크롤 버그 수정

--- src/main/resources/static/js/controller/IndexController.js
+++ src/main/resources/static/js/controller/IndexController.js
@@ -4,6 +4,8 @@
 
 var __IndexCtrl = function ($interval, $scope, $http, store, $state, $uibModal, $rootScope, $filter) {
     var userObject = store.get('obj');
+    $scope.quantity = 4;
+
     $scope.boardList = function () {
         $http({
             method: 'POST', //방식
